a438ff3e6ef13a4c5069e60454815f6dddf0411f
thomas ricci <grisou13@hotmail.fr>
2016-11-16 19:32:15 +0100

    added php5-dev

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-sudo apt-get install -y php5 php5-mbstring php5-json
+sudo apt-get install build-essential tcl
+sudo apt-get install -y php5 php5-dev php5-mbstring php5-json php5-xml php5-tokenizer php5-cli
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === 'aa96f26c2b67226a324c27919f1eb05f21c248b987e6195cad9690d5c1ff713d53020a02ac8c217dbf90a7eacc9d141d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php --filename=composer
 php -r "unlink('composer-setup.php');"
 #install redis
 #https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-redis-on-ubuntu-16-04
-sudo apt-get install build-essential tcl
 cd /tmp
 curl -O http://download.redis.io/redis-stable.tar.gz
 tar xzvf redis-stable.tar.gz
