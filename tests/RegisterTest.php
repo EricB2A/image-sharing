72373db7d0815eaba0ee5527278d6da36697fa78
Eric <paralysant@gmail.com>
2017-01-10 10:14:25 +0100

    Register test added

---
+++ tests/RegisterTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class RegisterTest extends TestCase
+{
+    /*
+     * Goes to the registration page from the root page
+     *
+     * @return void
+     */
+    public function testGoToResiter(){
+        $this->visit("/")
+            ->click("Register")
+            ->seePageIs("/register");
+    }
+
+    public function testRegisterEmptyForm(){
+        $this->visit("/register")
+             ->press("Register")
+             ->seePageIs("/register");
+    }
+
+    public function testRegisterWithoutEmail(){
+        $this->visit("/register")
+             ->type("Mo", "name")
+             ->type("1234", "password")
+             ->type("1234", "password_confirmation")
+             ->press("Register")
+             ->seePageIs("/register");
+    }
+
+    public function testRegisterDifferentPasswords(){
+        $this->visit("/register")
+             ->type("Momo", "name")
+             ->type("justin.timberlake@whitehouse.gov", "email")
+             ->type("123456789", "password")
+             ->type("secret1234", "password_confirmation")
+             ->press("Register")
+             ->see("The password confirmation does not match.");
+    }
+
+}
